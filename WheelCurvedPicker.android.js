b49e6699a9350adda6436bb70af2d9f0260046e9
sehranhas <sehran.hasanli@mail.utoronto.ca>
2017-12-06 00:43:06 -0500

    add fix for PropTypes

--- WheelCurvedPicker.android.js
+++ WheelCurvedPicker.android.js
@@ -6,6 +6,7 @@ import {
 	ColorPropType,
 	requireNativeComponent,
 } from 'react-native';
+import PropTypes from 'prop-types';
 
 
 var WheelCurvedPicker = React.createClass ({
@@ -13,21 +14,21 @@ var WheelCurvedPicker = React.createClass ({
 	propTypes: {
 		...View.propTypes,
 
-		data: React.PropTypes.array,
+		data: PropTypes.array,
 
 		textColor: ColorPropType,
 
-		textSize: React.PropTypes.number,
+		textSize: PropTypes.number,
 
-		itemStyle: React.PropTypes.object,
+		itemStyle: PropTypes.object,
 
-		itemSpace: React.PropTypes.number,
+		itemSpace: PropTypes.number,
 
-		onValueChange: React.PropTypes.func,
+		onValueChange: PropTypes.func,
 
-		selectedValue: React.PropTypes.any,
+		selectedValue: PropTypes.any,
 
-		selectedIndex: React.PropTypes.number,
+		selectedIndex: PropTypes.number,
 	},
 
 	getDefaultProps(): Object {
@@ -80,8 +81,8 @@ var WheelCurvedPicker = React.createClass ({
 
 WheelCurvedPicker.Item = React.createClass({
 	propTypes: {
-		value: React.PropTypes.any, // string or integer basically
-		label: React.PropTypes.string,
+		value: PropTypes.any, // string or integer basically
+		label: PropTypes.string,
 	},
 
 	render: function() {
